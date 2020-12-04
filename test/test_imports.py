3a0b41f656ca800ee1397c95d8192757331947b8
Daniel Correia <dan.reia@gmail.com>
2020-12-04 12:02:30 +0200

    Added unit test for imports

---
+++ test/test_imports.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestImports(unittest.TestCase):
+
+    def test_import_ga4mp(self):
+        test_pass=None
+        try:
+            from ga4mp import Ga4mp
+            from ga4mp.ga4mp import Ga4mp
+            test_pass=True
+        except:
+            test_pass=False
+        self.assertEqual(test_pass,True)
+
+if __name__ == '__main__':
+    unittest.main()
