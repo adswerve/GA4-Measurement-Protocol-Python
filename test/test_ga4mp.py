7d5b561fe4de75b672f85e1c979bdd8d944cccfa
RuslanBergenov <bergenov@ualberta.ca>
2021-01-25 14:23:21 -0700

    feat: validation and error handling if users pass events incorrectly

--- test/test_ga4mp.py
+++ test/test_ga4mp.py
@@ -2,6 +2,7 @@ import requests
 import json
 import copy
 import unittest
+import pytest
 
 from ga4mp import Ga4mp
 from ga4mp.utils import params_dict
@@ -13,6 +14,73 @@ logger = logging.getLogger(__name__)
 
 
 class Ga4mpTestMethods(unittest.TestCase):
+
+    def test_check_params_1_events_correct(self):
+
+        events_correct = [{'name': 'level_end',
+          'params': {'level_name': 'First',
+                     'success': 'True'}
+          },
+         {'name': 'level_up',
+          'params': {'character': 'John Madden',
+                     'level': 'First'}
+          }]
+
+        Ga4mp._check_params(self, events_correct)
+
+        assert True
+
+    def test_check_params_2_events_not_a_list(self):
+
+        events_not_a_list = ({'name': 'level_end',
+                   'params': {'level_name': 'First',
+                              'success': 'True'}
+                   },
+                  {'name': 'level_up',
+                   'params': {'character': 'John Madden',
+                              'level': 'First'}
+                   })
+        with pytest.raises(AssertionError, match="events should be a list"):
+            Ga4mp._check_params(self, events_not_a_list)
+
+    def test_check_params_3_event_not_a_dict(self):
+
+        events_event_not_a_dict = [{'name': 'level_end',
+                     'params': {'level_name': 'First',
+                                'success': 'True'}
+                     },
+                    ['name', 'level_up', 'params', {'character': 'John Madden',
+                                'level': 'First'}
+                     ]]
+
+        with pytest.raises(AssertionError, match="each event should be a dictionary"):
+            Ga4mp._check_params(self, events_event_not_a_dict)
+
+    def test_check_params_4_events_incorrect_key(self):
+
+        events_incorrect_key = [{'incorrect_key': 'level_end',
+                     'params': {'level_name': 'First',
+                                'success': 'True'}
+                     },
+                    {'name': 'level_up',
+                     'params': {'character': 'John Madden',
+                                'level': 'First'}
+                     }]
+
+        with pytest.raises(AssertionError, match="each event should have a name key"):
+            Ga4mp._check_params(self, events_incorrect_key)
+
+
+    def test_check_params_5_warning(self):
+
+        events_should_get_warning = [{'name': 'level_end',
+                                      'params' : {'level_name': 'First',
+                                                  'incorrect_key': 'True'}
+                                     }]
+
+
+        Ga4mp._check_params(self, events_should_get_warning)
+
     def test_http_status_code(self):
 
         # Create an instance of GA4 object
