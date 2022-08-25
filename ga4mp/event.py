1c0c162d4e47d37c5dbfba1d86cebc33bc3575ea
Ryan Ptomey <rptomey6@gmail.com>
2022-08-25 17:03:37 -0500

    event class validation functions

--- ga4mp/event.py
+++ ga4mp/event.py
@@ -1,8 +1,10 @@
 class Event(dict):
     def __init__(self, name):
         self.set_event_name(name)
-        
+
     def set_event_name(self, name):
+        if len(name) > 40:
+            raise ValueError("Event name cannot exceed 40 characters.")
         self["event_name"] = name
 
     def get_event_name(self):
@@ -12,3 +14,10 @@ class Event(dict):
         if "params" not in self.keys():
             self["params"] = {}
         self["params"][name] = value
+
+    def add_item(self, item):
+        if not isinstance(item, dict):
+            raise ValueError("'item' must be an instance of a dictionary.")
+        if "items" not in self["params"].keys():
+            self.set_event_param("items", [])
+        self["params"]["items"].append(item)
