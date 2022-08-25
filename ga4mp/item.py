73c019a5823e52729707c48fd93cf3e861d3e78b
Ryan Ptomey <rptomey6@gmail.com>
2022-08-25 16:46:49 -0500

    simplified item class functions

--- ga4mp/item.py
+++ ga4mp/item.py
@@ -2,11 +2,10 @@ class Item(dict):
     def __init__(self, item_id=None, item_name=None):
         if item_id is None and item_name is None:
             raise ValueError("At least one of 'item_id' and 'item_name' is required.")
-        self.set_item_id(item_id)
-        self.set_item_name(item_name)
+        if item_id is not None:
+            self.set_parameter("item_id", str(item_id))
+        if item_name is not None:
+            self.set_parameter("item_name", item_name)
 
-    def set_item_id(self, item_id):
-        self["item_id"] = item_id
-
-    def set_item_name(self, item_name):
-        self["item_name"] = item_name
+    def set_parameter(self, name, value):
+        self[name] = value
