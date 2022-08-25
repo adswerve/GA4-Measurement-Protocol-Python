fba7606c8985cf19184bb4e1c61103a2a6039bc6
Ryan Ptomey <rptomey6@gmail.com>
2022-08-25 16:33:56 -0500

    added init function to event

--- ga4mp/event.py
+++ ga4mp/event.py
@@ -1,9 +1,14 @@
 class Event(dict):
+    def __init__(self, name):
+        self.set_event_name(name)
+        
     def set_event_name(self, name):
         self["event_name"] = name
 
     def get_event_name(self):
         return self.get("event_name")
 
-    def set_event_param(self, name, value):
+    def set_event_param(self, name, value): # TODO: add validation functions for bad event names or parameters, etc.
+        if "params" not in self.keys():
+            self["params"] = {}
         self["params"][name] = value
